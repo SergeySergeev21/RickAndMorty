0bf4c0404f33cf081a66eefba3926a532b956d77
Sergey2195 <wowenjoyer2195@gmail.com>
2024-05-19 16:37:28 +0300

    ios swiftUi character cell

---
+++ shared/src/commonMain/kotlin/CharacterModel.kt
@@ -0,0 +1,9 @@
+data class CharacterModel(
+    val id: String,
+    val name: String,
+    val status: String,
+    val species: String,
+    val type: String,
+    val gender: String,
+    val image: String
+)
